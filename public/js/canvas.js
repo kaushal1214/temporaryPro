1e88fc12379827999512126ccd5cb4920cd71304
kaushal kishor <bonjourandroid@gmail.com>
2020-04-08 13:46:45 +0530

    Fixed a Major bug

--- public/js/canvas.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById('target');
-if(canvas){
-    let form = document.getElementById("canvasFingerprint");
-    form.value = canvas.toDataURL("image/png",0.5).split("base64,")[1].toString();
-}
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+var canvas = document.getElementById('target');
+if(canvas){
+    let form = document.getElementById("canvasFingerprint");
+    form.value = canvas.toDataURL("image/png",0.5).split("base64,")[1].toString();
+}
+
 function changeFocus(id){
     if(document.getElementById(`securitycode${id-1}`).value.length>0)
     document.getElementById(`securitycode${id}`).focus();
